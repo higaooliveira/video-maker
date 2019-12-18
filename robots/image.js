a1c5a7952703fb6360611332000e4cd5f1924ba5
Higor F. Oliveira <higoroliveiiira@gmail.com>
2019-12-18 16:09:58 -0300

    Download each image from sentence

--- robots/image.js
+++ robots/image.js
@@ -2,13 +2,17 @@ const google = require('googleapis').google
 const customSearch = google.customsearch('v1')
 const state = require('./state.js')
 const googleCredentials = require('../credentials/googleapi.json')
+const imageDownloader = require('image-downloader')
+
 
 async function robot(){
     const content = state.load()
 
-    await fetchImagesOfAllSentences(content)
+    // await fetchImagesOfAllSentences(content)
+    await downloadAllImages(content)
 
-    state.save(content)
+    // state.save(content)
+
     async function fetchImagesOfAllSentences(content){
         for (const sentence of content.sentences){
             const query = `${content.searchTerm} ${sentence.keywords[0]}`
@@ -30,6 +34,36 @@ async function robot(){
         return imagesUrl
     }
 
+    async function downloadAllImages(content){
+        content.downloadedImages = []
+        content.sentences[1].images[0] = 'https://liquipedia.net/commons/images/thumb/0/04/SKGaming.png/600px-SKGaming.png'
+
+        for(let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++){
+            const images = content.sentences[sentenceIndex].images
+            for(let imageIndex = 0; imageIndex < images.length; imageIndex++){
+                const imageUrl = images[imageIndex]
+
+                try{
+                    if(content.downloadedImages.includes(imageUrl)){
+                        throw new Error('Imagem já baixada')
+                    }
+                    await downloadAndSave(imageUrl, `${sentenceIndex}-original.png`)
+                    content.downloadedImages.push(imageUrl)
+                    console.log(`> [${sentenceIndex}]|[${imageIndex}] Baixou imagem com sucesso (${imageUrl})`)
+                    break
+                }catch(error){
+                    console.log(`> [${sentenceIndex}]|[${imageIndex}] Erro ao baixar (${imageUrl}): ${error}`)
+                }
+            }
+        }
+    }
+
+    async function downloadAndSave(imageUrl, fileName){
+        return imageDownloader.image({
+            url: imageUrl,
+            dest: `./content/${fileName}`
+        })
+    }
 }
 
 module.exports = robot
